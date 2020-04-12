1696ed9e0845a7d0724fcf237394ae2df3d9a77b
Jochen Dueckminor <jochen@dueckminor.de>
2020-04-12 20:41:56 +0200

    git rid of mypi-workspace

--- scripts/mypi-setup/start_local_js.sh
+++ scripts/mypi-setup/start_local_js.sh
@@ -4,18 +4,7 @@ set -ex
 
 DIR_MYPI_TOOLS="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.."; pwd)"
 
-if [[ ! -d "${DIR_MYPI_TOOLS}/../mypi-workspace/.git" ]]; then
-    echo
-    echo "Please do the following:"
-    echo 
-    echo "cd \"$(cd ${DIR_MYPI_TOOLS}/..)\""
-    echo "git clone git@github.com:dueckminor/mypi-workspace.git"
-    echo
-    exit 1
-fi
-
-DIR_MYPI_WORKSPACE="$(cd "${DIR_MYPI_TOOLS}/../mypi-workspace"; pwd)"
-DIR_MYPI_ROOT="${DIR_MYPI_WORKSPACE}/.mypi"
+#DIR_MYPI_ROOT="${DIR_MYPI_TOOLS}/.mypi"
 
 PID="$(pgrep -f " --port 9501" || true)"
 if [[ -n "${PID}" ]]; then
