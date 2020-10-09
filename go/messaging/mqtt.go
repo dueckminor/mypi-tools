9e7026c55529b7e67ebb336a9080a75a1caca15c
Jochen Dueckminor <jochen@dueckminor.de>
2020-10-09 21:55:31 +0200

    disabled bad code

--- go/messaging/mqtt.go
+++ go/messaging/mqtt.go
@@ -1,15 +1,10 @@
 package messaging
 
-import (
-	"github.com/dueckminor/mypi-tools/go/tlsconfig"
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-)
-
 func CreateClient() {
-	tlsconfig := tlsconfig.NewTLSConfig()
-	opts := mqtt.NewClientOptions()
-	opts.AddBroker("ssl://rpi:8883")
-	opts.SetClientID(hostname).SetTLSConfig(tlsconfig)
+	//tlsconfig := tlsconfig.NewTLSConfig()
+	//opts := mqtt.NewClientOptions()
+	//opts.AddBroker("ssl://rpi:8883")
+	//opts.SetClientID(hostname).SetTLSConfig(tlsconfig)
 
-	mqttClient := mqtt.NewClient(opts)
+	//mqttClient := mqtt.NewClient(opts)
 }
