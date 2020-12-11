6aed18f7346b7de57e611eb9ca58ae5df62a9aba
Jochen Dueckminor <jochen@dueckminor.de>
2020-12-11 18:53:21 +0100

    updated certificate downloader

--- cmd/mypi-setup/impl/certificates.go
+++ cmd/mypi-setup/impl/certificates.go
@@ -6,7 +6,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type CertInfo struct {
+	Text string `json:"text"`
+}
+
 // GetCertificates return list of hosts
 func GetCertificates(c *gin.Context) {
-	c.JSON(http.StatusOK, []string{"localhost", "mypi"})
+	c.JSON(http.StatusOK, []CertInfo{CertInfo{"localhost"}, CertInfo{"mypi"}})
 }
