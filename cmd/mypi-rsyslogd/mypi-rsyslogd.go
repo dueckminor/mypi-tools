6fcb7d28962c8fe83ec87932d7bcf53489a9c425
Jochen Dueckminor <jochen@dueckminor.de>
2021-05-18 21:11:16 +0200

    rsyslog

---
+++ cmd/mypi-rsyslogd/mypi-rsyslogd.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+)
+
+// maxBufferSize specifies the size of the buffers that
+// are used to temporarily hold data from the UDP packets
+// that we receive.
+const maxBufferSize = 1024
+
+// server wraps all the UDP echo server functionality.
+// ps.: the server is capable of answering to a single
+// client at a time.
+func server(ctx context.Context, address string) (err error) {
+	// ListenPacket provides us a wrapper around ListenUDP so that
+	// we don't need to call `net.ResolveUDPAddr` and then subsequentially
+	// perform a `ListenUDP` with the UDP address.
+	//
+	// The returned value (PacketConn) is pretty much the same as the one
+	// from ListenUDP (UDPConn) - the only difference is that `Packet*`
+	// methods and interfaces are more broad, also covering `ip`.
+	pc, err := net.ListenPacket("udp", address)
+	if err != nil {
+		return
+	}
+
+	// `Close`ing the packet "connection" means cleaning the data structures
+	// allocated for holding information about the listening socket.
+	defer pc.Close()
+
+	fmt.Println("Listen on:", pc.LocalAddr())
+
+	doneChan := make(chan error, 1)
+	buffer := make([]byte, maxBufferSize)
+
+	// Given that waiting for packets to arrive is blocking by nature and we want
+	// to be able of canceling such action if desired, we do that in a separate
+	// go routine.
+	go func() {
+		for {
+			// By reading from the connection into the buffer, we block until there's
+			// new content in the socket that we're listening for new packets.
+			//
+			// Whenever new packets arrive, `buffer` gets filled and we can continue
+			// the execution.
+			//
+			// note.: `buffer` is not being reset between runs.
+			//	  It's expected that only `n` reads are read from it whenever
+			//	  inspecting its contents.
+			n, addr, err := pc.ReadFrom(buffer)
+			if err != nil {
+				doneChan <- err
+				return
+			}
+
+			fmt.Printf("packet-received: bytes=%d from=%s: %s\n",
+				n, addr.String(), string(buffer[:n]))
+		}
+	}()
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("cancelled")
+		err = ctx.Err()
+	case err = <-doneChan:
+	}
+
+	return
+}
+
+func main() {
+	server(context.Background(), ":514")
+}
