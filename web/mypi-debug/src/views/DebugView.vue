f164ef07b8c8b79b9e53ac810882b00ba930596b
Jochen Dückminor <jochen@dueckminor.de>
2023-03-13 07:48:42 +0100

    next steps

--- web/mypi-debug/src/views/DebugView.vue
+++ web/mypi-debug/src/views/DebugView.vue
@@ -6,11 +6,18 @@ v-container
         v-expansion-panels(multiple)
           v-expansion-panel(v-for="component in service.components")
             v-expansion-panel-title
-              v-chip(v-if="component.state=='running'",color="green") {{ component.state }}
-              v-chip(v-else-if="component.state=='stopped'",color="red") {{ component.state }}
-              v-chip(v-else,color="orange") {{ component.state }}
-              v-btn(@click="restart_component(service.name,component.name)",@click.native.stop) restart
-              div - {{ component.name }}
+              div
+                v-chip(v-if="component.state=='running'",color="green") {{ component.state }}
+                v-chip(v-else-if="component.state=='stopped'",color="red") {{ component.state }}
+                v-chip(v-else,color="orange") {{ component.state }}
+              div &nbsp; {{ component.name }}
+              v-spacer
+              template(v-for="action in component.actions")
+                v-btn(v-if="action.name=='restart'" @click="call_action(service.name,component.name,action.name)",@click.native.stop)
+                  v-icon mdi-restart
+                v-btn(v-else-if="action.name=='debug'" @click="call_action(service.name,component.name,action.name)",@click.native.stop)
+                  v-icon mdi-bug-outline
+                v-btn(v-else @click="call_action(service.name,component.name,action.name)",@click.native.stop) {{action.name}}
             v-expansion-panel-text
               go-tty(:path="`/api/services/`+service.name+`/components/`+component.name+`/tty`" style="padding: 0")
 </template>
@@ -28,10 +35,10 @@ const services = storeToRefs(useServiceStore()).services
 //const services = useServiceStore()
 
 // eslint-disable-next-line no-unused-vars
-function restart_component(service,component) {
+function call_action(service,component,action) {
   // eslint-disable-next-line no-console
-  console.log("restart_component "+component);
-  axios({ method: "POST", url: "/api/services/"+service+"/components/"+component+"/restart" })
+  console.log("call_action "+component+action);
+  axios({ method: "POST", url: "/api/services/"+service+"/components/"+component+"/"+action })
 }
 </script>
 <!-- ----------------------------------------------------------------------- -->
