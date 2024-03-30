938be04432a91e3dd5b3ccd58343b78e5e950e83
Jochen Dückminor <jochen@dueckminor.de>
2024-03-30 14:06:02 +0100

    improved login ui

--- web/mypi-auth/src/App.vue
+++ web/mypi-auth/src/App.vue
@@ -1,47 +1,45 @@
 <template>
-  <v-app>
+  <v-app theme="dark">
     <v-main>
-      <v-container fluid fill-height align-center>
-        <v-layout align-center justify-center>
-            <v-card v-if="authenticated" class="elevation-12" max-height="500px" align-center>
-              <span>{{username}}</span>
-              <v-card-actions>
-                <v-btn color="primary" @click="logout">Logout</v-btn>
-              </v-card-actions>
-            </v-card>
-            <v-card v-if="!authenticated" class="elevation-12" max-height="500px" align-center>
-              <v-toolbar dark color="primary">
-                <v-toolbar-title>Login</v-toolbar-title>
-                <v-spacer></v-spacer>
-                <v-tooltip bottom>
-                </v-tooltip>
-              </v-toolbar>
-              <v-card-text>
-                <v-form>
-                  <div style="width:400px"></div>
-                  <v-text-field v-model="username" prepend-icon="mdi-account" name="login" label="Login" type="text" v-on:keyup.enter="$refs.passwordField.focus()"></v-text-field>
-                  <v-text-field v-model="password" prepend-icon="mdi-lock" name="password" label="Password" id="password" type="password" ref="passwordField" v-on:keyup.enter="login"></v-text-field>
-                </v-form>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="primary" @click="login">Login</v-btn>
-                <v-spacer></v-spacer>
-              </v-card-actions>
-            </v-card>
-        </v-layout>
-      </v-container>
+      <v-row fill-width>
+        <v-col/>
+        <v-col>
+          <v-container fluid fill-height fill-width align-center>
+            <v-layout align-center justify-center>
+              <v-card v-if="authenticated" class="elevation-12" max-height="500px" align-center style="margin: 25px;">
+                <v-toolbar dark color="primary">
+                  <v-toolbar-title>Logged in as '{{username}}'</v-toolbar-title>
+                </v-toolbar>
+                <div style="width:450px"></div>
+                <v-card-actions>
+                  <v-btn color="primary" @click="logout">Logout</v-btn>
+                </v-card-actions>
+              </v-card>
+              <v-card v-if="!authenticated" class="elevation-12" max-height="500px" align-center style="margin: 20px;">
+                <v-toolbar dark color="primary">
+                  <v-toolbar-title>Login</v-toolbar-title>
+                </v-toolbar>
+                <v-card-text>
+                  <v-form>
+                    <div style="width:450px"></div>
+                    <v-text-field v-model="username" prepend-icon="mdi-account" name="login" label="Login" type="text" v-on:keyup.enter="$refs.passwordField.focus()"></v-text-field>
+                    <v-text-field v-model="password" prepend-icon="mdi-lock" name="password" label="Password" id="password" type="password" ref="passwordField" v-on:keyup.enter="login"></v-text-field>
+                  </v-form>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" @click="login">Login</v-btn>
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+              <v-spacer/>
+            </v-layout>
+          </v-container>
+        </v-col>
+        <v-col/>
+      </v-row>
       <v-snackbar v-model="snackbar">
         {{ snackbar_text }}
-        <template v-slot:actions>
-          <v-btn
-            color="pink"
-            variant="text"
-            @click="snackbar = false"
-          >
-            Close
-          </v-btn>
-        </template>
       </v-snackbar>
     </v-main>
   </v-app>
@@ -101,9 +99,10 @@ export default {
               window.location = "oauth/authorize?client_id="+encodeURIComponent(this.clientId) +
                 "&redirect_uri=" + encodeURIComponent(this.redirectURI) +
                 "&response_type=" + encodeURIComponent(this.response_type)
+            } else {
+              this.authenticated = true
+              this.password = ""
             }
-            this.authenticated = true
-            this.password = ""
           }
         }
       )
